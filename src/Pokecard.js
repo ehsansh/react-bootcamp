52b4c4f57a74f717a19f0ecbfb15fee7fa85639f
ehsansh <ehsanshadmehri@gmail.com>
2022-05-16 12:02:41 +0430

    change image url and its logic

--- src/Pokecard.js
+++ src/Pokecard.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
 import './Pokecard.css';
-const img_api =
-    'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/';
+const img_api = ' https://assets.pokemon.com/assets/cms2/img/pokedex/detail/';
+
+const paddToThree = number =>
+    number <= 999 ? `00${number}`.slice(-3) : number;
 
 class PokeCard extends Component {
     render() {
         return (
             <div className='Pokecard'>
                 <div>{this.props.name}</div>
-                <img src={img_api + this.props.id + '.png'} />
+                <img src={img_api + paddToThree(this.props.id) + '.png'} />
                 <div>{this.props.type}</div>
                 <div>{this.props.exp}</div>
             </div>
