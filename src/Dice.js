92e2060c3c380f710f8a7b6a34d1eca449dfdc62
ehsansh <ehsanshadmehri@gmail.com>
2022-05-17 01:48:59 +0430

    add roll method

--- src/Dice.js
+++ src/Dice.js
@@ -1,39 +1,10 @@
 import React, { Component } from 'react';
-import {
-    faDiceOne,
-    faDiceTwo,
-    faDiceThree,
-    faDiceFour,
-    faDiceFive,
-    faDiceSix,
-} from '@fortawesome/free-solid-svg-icons';
+
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 class Dice extends Component {
     render() {
-        let icon;
-        switch (this.props.num) {
-            case 1:
-                icon = faDiceOne;
-                break;
-            case 2:
-                icon = faDiceTwo;
-                break;
-            case 3:
-                icon = faDiceThree;
-                break;
-            case 4:
-                icon = faDiceFour;
-                break;
-            case 5:
-                icon = faDiceFive;
-                break;
-            default:
-                icon = faDiceSix;
-                break;
-        }
-
-        return <FontAwesomeIcon icon={icon} size='6x' />;
+        return <FontAwesomeIcon icon={this.props.icon} size='6x' />;
     }
 }
 
--- src/RollDice.js
+++ src/RollDice.js
@@ -1,12 +1,43 @@
 import React, { Component } from 'react';
 import Dice from './Dice';
-
+import {
+    faDiceOne,
+    faDiceTwo,
+    faDiceThree,
+    faDiceFour,
+    faDiceFive,
+    faDiceSix,
+} from '@fortawesome/free-solid-svg-icons';
 class RollDice extends Component {
+    static defaultProps = {
+        faces: [
+            faDiceOne,
+            faDiceTwo,
+            faDiceThree,
+            faDiceFour,
+            faDiceFive,
+            faDiceSix,
+        ],
+    };
+    constructor(props) {
+        super(props);
+        this.state = {
+            a: 1,
+            b: 1,
+        };
+        this.roll = this.roll.bind(this);
+    }
+    roll() {
+        let rand1 = Math.floor(Math.random() * 6);
+        let rand2 = Math.floor(Math.random() * 6);
+        this.setState({ a: rand1, b: rand2 });
+    }
     render() {
         return (
             <div>
-                <Dice num={1} />
-                <Dice num={6} />
+                <Dice icon={this.props.faces[this.state.a]} />
+                <Dice icon={this.props.faces[this.state.b]} />
+                <button onClick={this.roll}>roll the dices</button>
             </div>
         );
     }
