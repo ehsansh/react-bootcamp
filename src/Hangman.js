7d41bde36892df7f92291807813fdf0b7ba07c24
ehsansh <ehsanshadmehri@gmail.com>
2022-05-20 22:53:21 +0430

    change alt variable with backticks

--- src/Hangman.js
+++ src/Hangman.js
@@ -58,7 +58,7 @@ class Hangman extends Component {
 
     /** render: render game */
     render() {
-        const altImg = this.state.nWrong + '/' + this.props.maxWrong;
+        const altImg = `${this.state.nWrong}/${this.props.maxWrong}`;
         return (
             <div className='Hangman'>
                 <h1>Hangman</h1>
