76d3c163d54b328dc7af8445d996cd758e982a36
ehsansh <ehsanshadmehri@gmail.com>
2022-05-22 20:31:43 +0430

    add style to cells

--- src/Cells.js
+++ src/Cells.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
+import './cells.css';
 
 class Cells extends Component {
     render() {
-        {
-            Array.from({ length: 25 }).map(e => {
-                return <div></div>;
-            });
-        }
+        const cells = Array.from({ length: 25 }).map((e, i) => {
+            return (
+                <div className='cell' key={i}>
+                    {i}
+                </div>
+            );
+        });
+        return <div className='cells-container'>{cells}</div>;
     }
 }
 
