d528daabbda4ffe54ef6e3f338d8166dfd702d35
ehsansh <ehsanshadmehri@gmail.com>
2022-05-22 20:51:39 +0430

    add click event for cells

--- src/Cells.js
+++ src/Cells.js
@@ -2,10 +2,24 @@ import React, { Component } from 'react';
 import './cells.css';
 
 class Cells extends Component {
+    constructor(props) {
+        super(props);
+        this.handleCellClick = this.handleCellClick.bind(this);
+    }
+    handleCellClick(ev) {
+        // console.log(ev.target.getAttribute('index'));
+        this.props.cellClick(ev.target.getAttribute('index'));
+    }
     render() {
+        const indexes = this.props.selectedCells;
         const cells = Array.from({ length: 25 }).map((e, i) => {
             return (
-                <div className='cell' key={i}>
+                <div
+                    className={`cell ${indexes.includes(i) && 'active'}`}
+                    key={i}
+                    index={i}
+                    onClick={this.handleCellClick}
+                >
                     {i}
                 </div>
             );
--- src/Game.js
+++ src/Game.js
@@ -3,11 +3,21 @@ import Cells from './Cells';
 class Game extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            selectedCells: [4, 18, 23],
+        };
+        this.cellClick = this.cellClick.bind(this);
+    }
+    cellClick(index) {
+        console.log(index);
     }
     render() {
         return (
             <div>
-                <Cells />
+                <Cells
+                    cellClick={this.cellClick}
+                    selectedCells={this.state.selectedCells}
+                />
             </div>
         );
     }
