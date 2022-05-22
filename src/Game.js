d9db75eb763811c590872f01ba6a06a983dc9bfe
ehsansh <ehsanshadmehri@gmail.com>
2022-05-23 00:23:07 +0430

    complete click event on cells to work correctly

--- src/Game.js
+++ src/Game.js
@@ -8,8 +8,28 @@ class Game extends Component {
         };
         this.cellClick = this.cellClick.bind(this);
     }
-    cellClick(index) {
-        console.log(index);
+    cellClick(val) {
+        let index = +val;
+        let toggleIndexes = [index];
+        let col = index % 5;
+        if (col > 0) toggleIndexes.push(index - 1);
+        if (col < 4) toggleIndexes.push(index + 1);
+        if (index - 5 > 0) toggleIndexes.push(index - 5);
+        if (index + 5 < 25) toggleIndexes.push(index + 5);
+
+        this.setState(st => {
+            return {
+                selectedCells: Array.from({ length: 25 })
+                    .map((el, i) => i)
+                    .filter(
+                        el =>
+                            (toggleIndexes.includes(el) &&
+                                !st.selectedCells.includes(el)) ||
+                            (!toggleIndexes.includes(el) &&
+                                st.selectedCells.includes(el))
+                    ),
+            };
+        });
     }
     render() {
         return (
