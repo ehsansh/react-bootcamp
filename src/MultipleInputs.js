b0b8c594df29a27fe9d9c673486fc3b30eeabd77
ehsansh <ehsanshadmehri@gmail.com>
2022-05-23 12:21:25 +0430

    create a form in react

---
+++ src/MultipleInputs.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+
+class MultipleInputs extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            username: '',
+            email: '',
+        };
+        this.handleChange = this.handleChange.bind(this);
+        this.formSubmit = this.formSubmit.bind(this);
+    }
+    handleChange(evt) {
+        this.setState({
+            [evt.target.name]: evt.target.value,
+        });
+    }
+    formSubmit(evt) {
+        evt.preventDefault();
+        alert(this.state.username);
+        this.setState({
+            username: '',
+            email: '',
+        });
+    }
+    render() {
+        return (
+            <div>
+                username is {this.state.username} <br />
+                email is {this.state.email}
+                <form>
+                    <input
+                        type='text'
+                        name='username'
+                        value={this.state.username}
+                        onChange={this.handleChange}
+                    />
+                    <input
+                        type='text'
+                        name='email'
+                        value={this.state.email}
+                        onChange={this.handleChange}
+                    />
+                    <button onClick={this.formSubmit}>submit</button>
+                </form>
+            </div>
+        );
+    }
+}
+
+export default MultipleInputs;
