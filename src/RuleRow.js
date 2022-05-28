5f18bae749c56921436451fa65934c32fd857ed5
ehsansh <ehsanshadmehri@gmail.com>
2022-05-28 11:29:59 +0430

    disable uppper and lower options

--- src/RuleRow.js
+++ src/RuleRow.js
@@ -1,15 +1,20 @@
 import React, { Component } from 'react';
-import './RuleRow.css'
+import './RuleRow.css';
 
 class RuleRow extends Component {
-  render() {
-    return (
-      <tr className="RuleRow RuleRow-active" onClick={this.props.doScore}>
-        <td className="RuleRow-name">{this.props.name}</td>
-        <td className="RuleRow-score">{this.props.score}</td>
-      </tr>
-    )
-  }
+    render() {
+        return (
+            <tr
+                className={`RuleRow RuleRow-${
+                    this.props.score === undefined ? 'active' : 'disabled'
+                }`}
+                onClick={this.props.score ?? this.props.doScore}
+            >
+                <td className='RuleRow-name'>{this.props.name}</td>
+                <td className='RuleRow-score'>{this.props.score}</td>
+            </tr>
+        );
+    }
 }
 
 export default RuleRow;
