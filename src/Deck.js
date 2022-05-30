822b2b3ec60c0e014d60fee96f735fa7419b539b
ehsansh <ehsanshadmehri@gmail.com>
2022-05-30 19:28:41 +0430

    add button for getting cards

--- src/Deck.js
+++ src/Deck.js
@@ -1,19 +1,50 @@
 import React, { Component } from 'react';
-const API_URL = 'https://deckofcardsapi.com/api/deck/new/shuffle';
+const API_BASE_URL = 'https://deckofcardsapi.com/api/deck';
 
 export default class Deck extends Component {
     constructor(props) {
         super(props);
         this.state = {
             deck: null,
+            drawn: [],
         };
+        this.getCard = this.getCard.bind(this);
     }
     async componentDidMount() {
-        const response = await fetch(API_URL);
+        const response = await fetch(`${API_BASE_URL}/new/shuffle`);
         const deck = await response.json();
         this.setState({ deck: deck });
     }
+    async getCard() {
+        try {
+            const id = this.state.deck.deck_id;
+            const card_url = `${API_BASE_URL}/${id}/draw/`;
+            const response = await fetch(card_url);
+            const data = await response.json();
+            if (!data.success) {
+                throw new Error('no cards left');
+            }
+            const card = data.cards[0];
+
+            this.setState(st => {
+                return {
+                    drawn: [
+                        ...st.drawn,
+                        {
+                            id: card.code,
+                            image: card.image,
+                        },
+                    ],
+                };
+            });
+        } catch (e) {}
+    }
     render() {
-        return <div>Deck</div>;
+        return (
+            <div>
+                <h1>deck</h1>
+                <button onClick={this.getCard}>Get Card</button>
+            </div>
+        );
     }
 }
