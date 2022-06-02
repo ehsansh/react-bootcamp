b383f0cb75d60f6442cb697f250e531a63f1e403
ehsansh <ehsanshadmehri@gmail.com>
2022-06-02 14:09:44 +0430

    add links for routing

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import React, { Component } from 'react';
 import './App.css';
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, Link } from 'react-router-dom';
 import Contact from './Contact';
 import About from './About';
+import Home from './Home';
 
 export default class App extends Component {
     render() {
         return (
             <div>
-                App
+                <nav>
+                    <Link to='/'>Home</Link>
+                    <Link to='/about'>About</Link>
+                    <Link to='/contact'>Contact</Link>
+                </nav>
                 <Routes>
+                    <Route exact path='/' element={<Home />} />
                     <Route exact path='/about' element={<About />} />
                     <Route exact path='/contact' element={<Contact />} />
                 </Routes>
