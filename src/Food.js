1127b1f753e43d2f9def8692c4d89e7c7d409afa
ehsansh <ehsanshadmehri@gmail.com>
2022-06-03 11:39:39 +0430

    redirect with Navigate

--- src/Food.js
+++ src/Food.js
@@ -1,12 +1,18 @@
 import { useParams } from 'react-router-dom';
-
+import { Navigate } from 'react-router-dom';
 function Food() {
     const { name } = useParams();
     const url = `https://source.unsplash.com/600x600/?${name}`;
     return (
         <div>
-            <h1>this is a {name}</h1>
-            <img src={url} alt={name} />
+            {/\d/.test(name) ? (
+                <Navigate replace to='/' />
+            ) : (
+                <div>
+                    <h1>this is a {name}</h1>
+                    <img src={url} alt={name} />
+                </div>
+            )}
         </div>
     );
 }
