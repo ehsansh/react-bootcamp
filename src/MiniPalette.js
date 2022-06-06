df61811665c0aba612f6569b91720bd3e7417717
ehsansh <ehsanshadmehri@gmail.com>
2022-06-06 23:57:59 +0430

    add minipalete and test css prop of emotion for styling

---
+++ src/MiniPalette.js
@@ -0,0 +1,31 @@
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
+
+function App() {
+    return (
+        <div>
+            <div
+                css={css({
+                    margin: 10,
+                    padding: 10,
+                    backgroundColor: '#eee',
+                })}
+            >
+                This is an example of <code>`css`</code> using an object.
+            </div>
+            <div
+                css={css`
+                    margin: 10px;
+                    padding: 10px;
+                    background-color: #cef;
+                    color: green;
+                `}
+            >
+                This is an example of <code>`css`</code> using a tagged template
+                literal.
+            </div>
+        </div>
+    );
+}
+
+export default App;
