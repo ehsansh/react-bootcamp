e00df8bac85106a5b3477e551f5658688c38c355
ehsansh <ehsanshadmehri@gmail.com>
2022-06-07 00:11:52 +0430

    test both styled components and css prop

--- src/MiniPalette.js
+++ src/MiniPalette.js
@@ -1,4 +1,6 @@
 import styled from '@emotion/styled';
+/** @jsxImportSource @emotion/react */
+import { css, jsx } from '@emotion/react';
 
 const Heading = styled('h1')`
     background-color: ${props => props.bg};
@@ -6,11 +8,25 @@ const Heading = styled('h1')`
 `;
 
 function App() {
+    const color = 'white';
     return (
         <div>
             <Heading bg='#008f68' fg='#fae042'>
                 Heading with a green background and yellow text.
             </Heading>
+            <div
+                css={css`
+                    padding: 32px;
+                    background-color: hotpink;
+                    font-size: 24px;
+                    border-radius: 4px;
+                    &:hover {
+                        color: ${color};
+                    }
+                `}
+            >
+                Hover to change color.
+            </div>
         </div>
     );
 }
