89475f21aa38424916563ae750f11311f2c478cc
ehsansh <ehsanshadmehri@gmail.com>
2022-06-06 23:59:20 +0430

    test styled components

--- src/MiniPalette.js
+++ src/MiniPalette.js
@@ -1,29 +1,16 @@
-/** @jsxImportSource @emotion/react */
-import { css } from '@emotion/react';
+import styled from '@emotion/styled';
+
+const Heading = styled('h1')`
+    background-color: ${props => props.bg};
+    color: ${props => props.fg};
+`;
 
 function App() {
     return (
         <div>
-            <div
-                css={css({
-                    margin: 10,
-                    padding: 10,
-                    backgroundColor: '#eee',
-                })}
-            >
-                This is an example of <code>`css`</code> using an object.
-            </div>
-            <div
-                css={css`
-                    margin: 10px;
-                    padding: 10px;
-                    background-color: #cef;
-                    color: green;
-                `}
-            >
-                This is an example of <code>`css`</code> using a tagged template
-                literal.
-            </div>
+            <Heading bg='#008f68' fg='#fae042'>
+                Heading with a green background and yellow text.
+            </Heading>
         </div>
     );
 }
