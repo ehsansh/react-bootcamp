83d1387842e6d750bf8c2be891c5a1b339de01ad
ehsansh <ehsanshadmehri@gmail.com>
2022-06-16 16:25:43 +0430

    refactor styles of colorbox

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -1,8 +1,63 @@
 import React, { Component } from 'react';
+import styled from '@emotion/styled';
 import { CopyToClipboard } from 'react-copy-to-clipboard';
 import { Link } from 'react-router-dom';
 import chroma from 'chroma-js';
 import './ColorBox.css';
+
+const CopyText = styled.p`
+    color: ${props =>
+        chroma(props.background).luminance() >= 0.7 ? 'black' : 'white'};
+`;
+
+const ColorName = styled.span`
+    color: ${props =>
+        chroma(props.background).luminance() <= 0.08 ? 'white' : 'black'};
+`;
+const SeeMore = styled.span`
+    color: ${props =>
+        chroma(props.background).luminance() >= 0.7
+            ? 'rgba(0,0,0,0.6)'
+            : 'white'};
+    background-color: rgba(255, 255, 255, 0.3);
+    position: absolute;
+    bottom: 0;
+    right: 0;
+    border: none;
+    width: 60px;
+    height: 30px;
+    line-height: 30px;
+    text-align: center;
+    text-transform: uppercase;
+`;
+const CopyButton = styled.button`
+    color: ${props =>
+        chroma(props.background).luminance() >= 0.7
+            ? 'rgba(0,0,0,0.6)'
+            : 'white'};
+    width: 100px;
+    height: 30px;
+    position: absolute;
+    display: inline-block;
+    top: 50%;
+    left: 50%;
+    margin-left: -50px;
+    margin-top: -15px;
+    text-align: center;
+    outline: none;
+    background: rgba(255, 255, 255, 0.3);
+    font-size: 1rem;
+    line-height: 30px;
+    text-transform: uppercase;
+    border: none;
+    text-decoration: none;
+    opacity: 0;
+    &:hover {
+        opacity: 1;
+        transition: 0.5s;
+    }
+`;
+
 export default class ColorBox extends Component {
     constructor(props) {
         super(props);
@@ -21,8 +76,6 @@ export default class ColorBox extends Component {
     render() {
         const { name, background, paletteId, id, showLink } = this.props;
         let { copied } = this.state;
-        const isDarkColor = chroma(background).luminance() <= 0.08;
-        const isLightColor = chroma(background).luminance() > 0.7;
 
         return (
             <CopyToClipboard text={background} onCopy={this.changeCopyState}>
@@ -33,34 +86,25 @@ export default class ColorBox extends Component {
                     ></div>
                     <div className={`copy-msg ${copied && 'show'}`}>
                         <h1>Copied!</h1>
-                        <p className={isLightColor && 'dark-text'}>
+                        <CopyText background={background}>
                             {background}
-                        </p>
+                        </CopyText>
                     </div>
                     <div className='copy-container'>
                         <div className='box-content'>
-                            <span className={isDarkColor && 'light-text'}>
+                            <ColorName background={background}>
                                 {name}
-                            </span>
+                            </ColorName>
                         </div>
                     </div>
-                    <button
-                        className={`copy-button ${isLightColor && 'dark-text'}`}
-                    >
-                        Copy
-                    </button>
+                    <CopyButton background={background}>Copy</CopyButton>
+
                     {showLink && (
                         <Link
                             to={`/palette/${paletteId}/${id}`}
                             onClick={e => e.stopPropagation()}
                         >
-                            <span
-                                className={`see-more ${
-                                    isLightColor && 'dark-text'
-                                }`}
-                            >
-                                MORE
-                            </span>
+                            <SeeMore background={background}>MORE</SeeMore>
                         </Link>
                     )}
                 </div>
