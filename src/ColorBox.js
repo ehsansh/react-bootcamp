57e281608f8bfd62662aa16bc47872a91dcf5d7e
ehsansh <ehsanshadmehri@gmail.com>
2022-06-17 12:12:14 +0430

    refactor more styles of the colorbox

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -5,6 +5,20 @@ import { Link } from 'react-router-dom';
 import chroma from 'chroma-js';
 import './ColorBox.css';
 
+const ColorBoxContainer = styled.div`
+    height: ${props => (props.showLink ? '25%' : '50%')};
+    width: 20%;
+    margin: 0 auto;
+    display: inline-block;
+    position: relative;
+    cursor: pointer;
+    margin-bottom: -3.5px;
+    &:hover button {
+        opacity: 1;
+        transition: 0.5s;
+    }
+`;
+
 const CopyText = styled.p`
     color: ${props =>
         chroma(props.background).luminance() >= 0.7 ? 'black' : 'white'};
@@ -52,9 +66,64 @@ const CopyButton = styled.button`
     border: none;
     text-decoration: none;
     opacity: 0;
-    &:hover {
+`;
+const BoxContent = styled.div`
+    position: absolute;
+    padding: 10px;
+    left: 0;
+    bottom: 0;
+    letter-spacing: 1px;
+    text-transform: uppercase;
+    font-size: 12px;
+`;
+const CopyOverlay = styled.div`
+    opacity: 0;
+    z-index: 0;
+    width: 100%;
+    height: 100%;
+    transition: transform 0.6s ease-in-out;
+    transform: scale(0.1);
+    &.show {
         opacity: 1;
-        transition: 0.5s;
+        transform: scale(50);
+        z-index: 3;
+        position: absolute;
+    }
+`;
+const CopyMessage = styled.div`
+    position: fixed;
+    left: 0;
+    right: 0;
+    top: 0;
+    bottom: 0;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    font-size: 4rem;
+    transform: scale(0.1);
+    opacity: 0;
+    color: white;
+    &.show {
+        opacity: 1;
+        transform: scale(1);
+        z-index: 9;
+        transition: all 0.4s ease-in-out;
+        transition-delay: 0.3s;
+    }
+    & h1 {
+        font-weight: 400;
+        text-shadow: 1px 2px black;
+        background-color: rgba(255, 255, 255, 0.3);
+        width: 100%;
+        text-align: center;
+        margin-bottom: none;
+        padding: 1rem;
+        text-transform: uppercase;
+    }
+    & p {
+        font-size: 2rem;
+        font-weight: 100;
     }
 `;
 
@@ -79,23 +148,25 @@ export default class ColorBox extends Component {
 
         return (
             <CopyToClipboard text={background} onCopy={this.changeCopyState}>
-                <div style={{ background }} className='ColorBox'>
-                    <div
+                <ColorBoxContainer showLink={showLink} style={{ background }}>
+                    <CopyOverlay
                         style={{ background }}
-                        className={`copy-overlay ${copied && 'show'}`}
-                    ></div>
-                    <div className={`copy-msg ${copied && 'show'}`}>
+                        className={`${copied && 'show'}`}
+                    ></CopyOverlay>
+
+                    <CopyMessage className={`${copied && 'show'}`}>
                         <h1>Copied!</h1>
                         <CopyText background={background}>
                             {background}
                         </CopyText>
-                    </div>
-                    <div className='copy-container'>
-                        <div className='box-content'>
+                    </CopyMessage>
+
+                    <div>
+                        <BoxContent>
                             <ColorName background={background}>
                                 {name}
                             </ColorName>
-                        </div>
+                        </BoxContent>
                     </div>
                     <CopyButton background={background}>Copy</CopyButton>
 
@@ -107,7 +178,7 @@ export default class ColorBox extends Component {
                             <SeeMore background={background}>MORE</SeeMore>
                         </Link>
                     )}
-                </div>
+                </ColorBoxContainer>
             </CopyToClipboard>
         );
     }
