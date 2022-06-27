d6b63d611bdd080249c879225878bc361b0798c4
Ehsan Shadmehri <ehsanshadmehri@gmail.com>
2022-06-27 15:42:26 +0430

    complete toggle hook

--- src/Toggler.js
+++ src/Toggler.js
@@ -1,9 +1,14 @@
-import React, { useState } from 'react';
+import React from 'react';
+import useToggle from './hooks/useToggle';
 
 export default function Toggler() {
-    const [isHappy, setIsHappy] = useState(true);
-    const toggle = () => {
-        setIsHappy(!isHappy);
-    };
-    return <h1 onClick={toggle}> {isHappy ? 'happy' : 'sad'} </h1>;
+    const [isHappy, toggleIsHappy] = useToggle(true);
+    const [isFresh, toggleIsFresh] = useToggle(true);
+
+    return (
+        <div>
+            <h1 onClick={toggleIsHappy}> {isHappy ? 'happy' : 'sad'} </h1>
+            <h1 onClick={toggleIsFresh}> {isFresh ? 'fresh' : 'tired'}</h1>
+        </div>
+    );
 }
