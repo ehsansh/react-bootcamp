d4aaa8a795eadeefbeba8e7d1c00aa3d628e5b58
Ehsan Shadmehri <ehsanshadmehri@gmail.com>
2022-07-02 15:05:02 +0430

    use useContext in form component and convert form to functional component

--- src/Form.js
+++ src/Form.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useContext } from 'react';
 import FormControl from '@mui/material/FormControl';
 import Paper from '@mui/material/Paper';
 import Avatar from '@mui/material/Avatar';
@@ -13,52 +13,47 @@ import Button from '@mui/material/Button';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import { LanguageContext } from './contexts/LanguageContext';
 
-export default class Form extends Component {
-    static contextType = LanguageContext;
-    render() {
-        const { language, changeLanguage } = this.context;
-        return (
-            <main>
-                <Paper>
-                    <p>language is : {language}</p>
-                    <Avatar>
-                        <LockOutlinedIcon></LockOutlinedIcon>
-                    </Avatar>
-                    <Typography variant='h5'>sign in</Typography>
-                    <Select value={language} onChange={changeLanguage}>
-                        <MenuItem value='english'>English</MenuItem>
-                        <MenuItem value='persian'>Persian</MenuItem>
-                        <MenuItem value='french'>French</MenuItem>
-                    </Select>
-                    <form>
-                        <FormControl margin='normal' required fullWidth>
-                            <InputLabel htmlFor='email'>Email</InputLabel>
-                            <Input id='email' name='email' autoFocus></Input>
-                        </FormControl>
-                        <FormControl margin='normal' required fullWidth>
-                            <InputLabel htmlFor='password'>password</InputLabel>
-                            <Input
-                                id='password'
-                                name='password'
-                                autoFocus
-                            ></Input>
-                        </FormControl>
-                        <FormControlLabel
-                            control={
-                                <Checkbox color='primary' label='remember me' />
-                            }
-                        />
-                    </form>
-                    <Button
-                        variant='contained'
-                        type='submit'
-                        fullWidth
-                        color='primary'
-                    >
-                        Sign in
-                    </Button>
-                </Paper>
-            </main>
-        );
-    }
+function Form(props) {
+    const { language, changeLanguage } = useContext(LanguageContext);
+    return (
+        <main>
+            <Paper>
+                <p>language is : {language}</p>
+                <Avatar>
+                    <LockOutlinedIcon></LockOutlinedIcon>
+                </Avatar>
+                <Typography variant='h5'>sign in</Typography>
+                <Select value={language} onChange={changeLanguage}>
+                    <MenuItem value='english'>English</MenuItem>
+                    <MenuItem value='persian'>Persian</MenuItem>
+                    <MenuItem value='french'>French</MenuItem>
+                </Select>
+                <form>
+                    <FormControl margin='normal' required fullWidth>
+                        <InputLabel htmlFor='email'>Email</InputLabel>
+                        <Input id='email' name='email' autoFocus></Input>
+                    </FormControl>
+                    <FormControl margin='normal' required fullWidth>
+                        <InputLabel htmlFor='password'>password</InputLabel>
+                        <Input id='password' name='password' autoFocus></Input>
+                    </FormControl>
+                    <FormControlLabel
+                        control={
+                            <Checkbox color='primary' label='remember me' />
+                        }
+                    />
+                </form>
+                <Button
+                    variant='contained'
+                    type='submit'
+                    fullWidth
+                    color='primary'
+                >
+                    Sign in
+                </Button>
+            </Paper>
+        </main>
+    );
 }
+
+export default Form;
