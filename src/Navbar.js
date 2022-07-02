0c073c6fa77ed0c34e67559dba524857ab5db7e6
Ehsan Shadmehri <ehsanshadmehri@gmail.com>
2022-07-02 15:13:07 +0430

    convert navbar to functional component and use useContext in it

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useContext } from 'react';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import IconButton from '@mui/material/IconButton';
@@ -7,34 +7,31 @@ import InputBase from '@mui/material/InputBase';
 import SearchIcon from '@mui/icons-material/Search';
 import Switch from '@mui/material/Switch';
 import { ThemeContext } from './contexts/ThemeContext';
-import { withLanguageContext } from './contexts/LanguageContext';
-class Navbar extends Component {
-    static contextType = ThemeContext;
-    render() {
-        const { isDarkMode, toggleTheme } = this.context;
-        const { language } = this.props.languageContext;
-        return (
-            <div>
-                <AppBar
-                    position='static'
-                    color={isDarkMode ? 'default' : 'primary'}
-                >
-                    <Toolbar>
-                        <IconButton color='inherit'></IconButton>
-                        <Typography variant='h6' color='inherit'>
-                            App Title {language}
-                        </Typography>
-                        <Switch onClick={toggleTheme} />
-                        <div className='search'>
-                            <div className='searhcIcon'>
-                                <SearchIcon />
-                            </div>
-                            <InputBase placeholder='Search...' />
+import { LanguageContext } from './contexts/LanguageContext';
+function Navbar(props) {
+    const { isDarkMode, toggleTheme } = useContext(ThemeContext);
+    const { language } = useContext(LanguageContext);
+    return (
+        <div>
+            <AppBar
+                position='static'
+                color={isDarkMode ? 'default' : 'primary'}
+            >
+                <Toolbar>
+                    <IconButton color='inherit'></IconButton>
+                    <Typography variant='h6' color='inherit'>
+                        App Title {language}
+                    </Typography>
+                    <Switch onClick={toggleTheme} />
+                    <div className='search'>
+                        <div className='searhcIcon'>
+                            <SearchIcon />
                         </div>
-                    </Toolbar>
-                </AppBar>
-            </div>
-        );
-    }
+                        <InputBase placeholder='Search...' />
+                    </div>
+                </Toolbar>
+            </AppBar>
+        </div>
+    );
 }
-export default withLanguageContext(Navbar);
+export default Navbar;
