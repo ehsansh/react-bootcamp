bded7da0dd76237f2050d2aecc948eb9c0fdecec
Ehsan Shadmehri <ehsanshadmehri@gmail.com>
2022-07-09 13:24:42 +0430

    make counter with useReducer

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import { useReducer } from 'react';
+
+function countReducer(state, action) {
+    switch (action.type) {
+        case 'increment':
+            return { count: state.count + action.amount };
+        case 'decrement':
+            return { count: state.count - 1 };
+        case 'reset':
+            return { count: 0 };
+    }
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [state, dispatch] = useReducer(countReducer, { count: 0 });
+    return (
+        <div className='App'>
+            <h1> {state.count} </h1>
+            <button onClick={() => dispatch({ type: 'increment', amount: 1 })}>
+                Add 1
+            </button>
+            <button onClick={() => dispatch({ type: 'increment', amount: 5 })}>
+                Add 5
+            </button>
+            <button onClick={() => dispatch({ type: 'decrement' })}>
+                subtract 1
+            </button>
+            <button onClick={() => dispatch({ type: 'reset' })}>reset</button>
+        </div>
+    );
 }
 
 export default App;
