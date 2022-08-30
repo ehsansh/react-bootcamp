ae706d3d3abc8e34e5c27725255473f2f60fccab
Ehsan Shadmehri <ehsanshadmehri@gmail.com>
2022-08-30 11:48:36 +0430

    start new project from scratch

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
-import Counter from './features/counter/Counter';
+import './App.css';
 
 function App() {
-    return (
-        <div className='App'>
-            <Counter />
-        </div>
-    );
+    return <div className='App'></div>;
 }
 
 export default App;
