9f685d4b9faa4ee299e9c62a4ccfe170c617d9fb
Ehsan Shadmehri <ehsanshadmehri@gmail.com>
2023-01-24 00:18:43 +0330

    consume authcontext in navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,20 @@
 import React, { useContext } from 'react';
 
 import { ThemeContext } from '../context/ThemeContext';
-
+import { AuthContext } from '../context/AuthContext';
 const Navbar = () => {
-    const value = useContext(ThemeContext);
-    const { isLightTheme, light, dark } = value;
+    const themeValue = useContext(ThemeContext);
+    const { isLightTheme, light, dark } = themeValue;
     const theme = isLightTheme ? light : dark;
+
+    const { isAuthenticated, toggleAuth } = useContext(AuthContext);
 
     return (
         <div style={{ background: theme.bg, color: theme.color }}>
             <h1>context app</h1>
+            <h3 onClick={toggleAuth}>
+                {isAuthenticated ? 'logged in' : 'logged out'}
+            </h3>
             <ul>
                 <li style={{ background: theme.ui }}>Home</li>
                 <li style={{ background: theme.ui }}>About</li>
--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -4,7 +4,7 @@ export const AuthContext = createContext();
 
 export const AuthContextProvider = ({ children }) => {
     const [isAuthenticated, setIsAuthenticated] = useState(false);
-    toggleAuth = () => setIsAuthenticated(!isAuthenticated);
+    const toggleAuth = () => setIsAuthenticated(!isAuthenticated);
     return (
         <AuthContext.Provider value={{ isAuthenticated, toggleAuth }}>
             {children}
