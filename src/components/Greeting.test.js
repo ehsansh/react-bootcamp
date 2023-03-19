0e49091abd713ff51ba6bd3657bda7a9b63cf083
Ehsan Shadmehri <ehsanshadmehri@gmail.com>
2023-03-19 21:17:39 +0330

    write 3 tests for clicking in greeting component

--- src/components/Greeting.test.js
+++ src/components/Greeting.test.js
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import Greeting from './Greeting';
 
 describe('greeting component', () => {
@@ -11,5 +12,38 @@ describe('greeting component', () => {
         //assert
         const helloWorldElm = screen.getByText('hello world', { exact: false });
         expect(helloWorldElm).toBeInTheDocument();
+    });
+
+    test('renders good to see you if the button is NOT clicked', () => {
+        render(<Greeting />);
+
+        const goodToSeeElm = screen.getByText('good to see you.', {
+            exact: false,
+        });
+        expect(goodToSeeElm).toBeInTheDocument();
+    });
+
+    test('renders changed if the button is clicked', () => {
+        render(<Greeting />);
+
+        const buttonElm = screen.getByRole('button');
+        userEvent.click(buttonElm);
+
+        const changedTexElm = screen.getByText('changed.', {
+            exact: false,
+        });
+        expect(changedTexElm).toBeInTheDocument();
+    });
+
+    test('renders not showing good to see you if the button is clicked', () => {
+        render(<Greeting />);
+
+        const buttonElm = screen.getByRole('button');
+        userEvent.click(buttonElm);
+
+        const outputElm = screen.queryByText('good to see you', {
+            exact: false,
+        });
+        expect(outputElm).toBeNull();
     });
 });
