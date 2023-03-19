a8eba0cf1907f747344dc3a367e9da8070acec51
Ehsan Shadmehri <ehsanshadmehri@gmail.com>
2023-03-19 20:55:14 +0330

    add change text functionality to greeting

--- src/components/Greeting.js
+++ src/components/Greeting.js
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Greeting = () => {
+    const [text, setText] = useState(false);
+
+    const changeTextHandler = () => {
+        setText(true);
+    };
+
     return (
         <div>
             <h2>Hello World</h2>
-            <p>It's good to see you.</p>
+            {text ? <p>Changed.</p> : <p>It's good to see you.</p>}
+            <button onClick={changeTextHandler}>change text</button>
         </div>
     );
 };
